448ba4a6db3f93e5acd3b5afff3c99139f2f933b
petrovladislavovichmaxim-dev <petrovladislavovichmaxim@gmail.com>
2026-09-02 02:01:36 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PetrovLab4Variant3"
+include(":app")
